aa282b8bb6d447930ec0214ed14caaa0df72a583
moch1772 <ziggs1772@gmail.com>
2020-09-04 11:51:50 +0200

    editor is DONE and sortet files in test file

--- test/RealSites/Loggin.php
+++ test/RealSites/Loggin.php
@@ -8,7 +8,7 @@ $_SESSION["Login"]=FALSE;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="TEST.css">
+    <link rel="stylesheet" href="CSS/TEST.css">
     <title>Document</title>
 </head>
 <body>
--- test/RealSites/add/addKategori.php
+++ test/RealSites/add/addKategori.php
@@ -8,19 +8,20 @@ $database = "webshop";
 // Skapa kopling till server
 $mysqli = new mysqli($servername,$username,$password,$database);
 
+$kategori=$_POST["kategori"];
+
 $data;
     if(isset($_GET["kategori"]))
     {
         $data = $_GET["kategori"];
     }
     if($data==null){
-        header('Location: ' . $_SERVER['HTTP_REFERER']);
+        //header('Location: ' . $_SERVER['HTTP_REFERER']);
     }
 
 
 //Skapa kommandot för att hämta användarnamn och lösenord för servern
-$sql = "DELETE FROM specifikation WHERE ID=$data";
-//echo $sql;
+$sql = "insert into specifikation(product_ID,product_specifikation) VALUES($data, '$kategori')";
 
 //Aktivera komandot
 $query = $mysqli->query($sql);
